7a677a66e48d0c84e071026d08552623da0a9542
jetong <jose.e.tong@gmail.com>
2018-08-03 13:58:53 -0700

    finish by adding scroll-to effect

--- notes/javascript/udemy/the_react_practice_course_learn_by_building_projects/the_venue/src/App.js
+++ notes/javascript/udemy/the_react_practice_course_learn_by_building_projects/the_venue/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './resources/styles.css'
+import { Element } from 'react-scroll';
 
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';   // index.js is implicit here
@@ -7,22 +8,40 @@ import VenueNfo from './components/venueNfo';
 import Highlight from './components/Highlights';
 import Pricing from './components/pricing';
 import Location from './components/location';
+import Footer from './components/header_footer/Footer';
 
 class App extends Component {
-  render() {
-    return (
-      // temporarily hardcode style here to test AppBar transparency effect on scroll
-      // <div className="App" style={{ height:"1500px", background:'cornflowerblue'}}>
-      <div className="App">
-        <Header/>
-        <Featured/>
-        <VenueNfo/>
-        <Highlight/>
-        <Pricing/>
-        <Location/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			// temporarily hardcode style here to test AppBar transparency effect on scroll
+			// <div className="App" style={{ height:"1500px", background:'cornflowerblue'}}>
+			<div className="App">
+				<Header />
+
+				<Element name='featured'>
+					<Featured />
+				</Element>	
+				
+				<Element name='venuenfo'>
+					<VenueNfo />
+				</Element>
+
+				<Element name='highlights'>
+					<Highlight />
+				</Element>
+
+				<Element name='pricing'>
+					<Pricing />
+				</Element>
+
+				<Element name='location'>
+					<Location />
+				</Element>
+
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
--- notes/javascript/udemy/the_react_practice_course_learn_by_building_projects/the_venue/src/components/header_footer/SideDrawer.js
+++ notes/javascript/udemy/the_react_practice_course_learn_by_building_projects/the_venue/src/components/header_footer/SideDrawer.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { scroller } from 'react-scroll';
 
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 
 const SideDrawer = (props) => {
+
+    const scrollToElement = (element) => {
+        // scrollTo() takes an elementReference and props.
+        scroller.scrollTo(element, {
+            duration: 1500,
+            delay: 100,
+            smooth: true,
+            offset: -80    // adjust for fixed header covering
+        });
+        props.onClose(false);   // close drawer on selection
+    }
+
     return (
         <Drawer
             anchor="right"      // anchors Drawer to the right of screen
@@ -13,23 +26,23 @@ const SideDrawer = (props) => {
         >
             <List component="nav">
                 {/* button gives the item mouseover effects */}
-                <ListItem button onClick={()=> console.log('Featured')}>
+                <ListItem button onClick={()=> scrollToElement('featured')}>
                     Event starts in
                 </ListItem>
 
-                <ListItem button onClick={()=> console.log('Venue INFO')}>
+                <ListItem button onClick={()=> scrollToElement('venuenfo')}>
                     Venue INFO
                 </ListItem>
 
-                <ListItem button onClick={()=> console.log('Highlights')}>
+                <ListItem button onClick={()=> scrollToElement('highlights')}>
                     Highlights
                 </ListItem>
 
-                <ListItem button onClick={()=> console.log('Pricing')}>
+                <ListItem button onClick={()=> scrollToElement('pricing')}>
                     Pricing
                 </ListItem>
 
-                <ListItem button onClick={()=> console.log('Location')}>
+                <ListItem button onClick={()=> scrollToElement('location')}>
                     Location
                 </ListItem>
 
