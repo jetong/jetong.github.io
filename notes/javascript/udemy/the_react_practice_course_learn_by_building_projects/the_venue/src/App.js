5fce830c04a4c763a0544cd5f1aa6806ca6ebae0
jetong <jose.e.tong@gmail.com>
2018-08-03 00:32:39 -0700

    added Highlights section

--- notes/javascript/udemy/the_react_practice_course_learn_by_building_projects/the_venue/src/App.js
+++ notes/javascript/udemy/the_react_practice_course_learn_by_building_projects/the_venue/src/App.js
@@ -4,15 +4,18 @@ import './resources/styles.css'
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';   // index.js is implicit here
 import VenueNfo from './components/venueNfo';
+import Highlight from './components/Highlights';
 
 class App extends Component {
   render() {
     return (
       // temporarily hardcode style here to test AppBar transparency effect on scroll
-      <div className="App" style={{ height:"1500px", background:'cornflowerblue'}}>
+      // <div className="App" style={{ height:"1500px", background:'cornflowerblue'}}>
+      <div className="App">
         <Header/>
         <Featured/>
         <VenueNfo/>
+        <Highlight/>
       </div>
     );
   }
